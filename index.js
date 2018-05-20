86913222cd46e392edce0a709e6008b7b4b0551b
Shun Yao <ysmiracle@hotmail.com>
2018-05-20 02:27:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+const token = '065d3b08c94396a8eddfa50375264d4eee4253e0';
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
